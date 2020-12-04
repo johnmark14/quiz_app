78e95c704ff04fb7f5f4cb88c7b3732c25e6f7cc
johnmark14 <johmarkgabrentina@gmail.com>
2020-12-04 16:57:02 +0800

    Add initial question text and answer buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,71 @@ class QuizApp extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Center(
-              child: Text('Hello world'),
-            ),
+            child: new QuizPage(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              onPressed: () {
+                //The user picked true
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              onPressed: () {
+                // The user picked false
+              },
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        // TODO: Add a row here for score
+      ],
     );
   }
 }
