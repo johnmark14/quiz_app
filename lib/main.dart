c1b415d762c99d6353887be41292f93b607ef838
johnmark14 <johmarkgabrentina@gmail.com>
2020-12-04 16:20:21 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Center(
+              child: Text('Hello world'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
